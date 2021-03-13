899efc929655b66f3fd2993871339c8f1744392a
Shreeya Gupta <shreeyag11@gmail.com>
2021-03-13 11:49:03 -0500

    Find the height of a binary tree

--- src/com/company/Tree.java
+++ src/com/company/Tree.java
@@ -71,6 +71,22 @@ public class Tree {
         System.out.print(root.value +" ");
     }
 
+    public void height() {
+        height(root);
+    }
+
+    private int height(Node root) {
+        if(root == null)
+            return -1;
+
+        if(root.left == null && root.right == null)
+            return 0;
+
+        return 1 + Math.max(
+                height(root.left),
+                height(root.right));
+    }
+
     public void inOrder() {
         System.out.println("In-order: ");
         inOrder(root);
