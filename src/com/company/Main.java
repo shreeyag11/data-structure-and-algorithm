2e53b5d82ff4c721ef381bbd0f4658fd5dcf3f0c
Shreeya Gupta <shreeyag11@gmail.com>
2021-03-20 17:09:21 -0400

    Check if a string is palindrome

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(StringManipulation.areAnagram("abcd", "bcads"));
+        System.out.println(StringManipulation.isPalindrome(""));
 
     }
 
--- src/com/company/StringManipulation.java
+++ src/com/company/StringManipulation.java
@@ -146,4 +146,16 @@ public class StringManipulation {
 
             return true;
     }
+
+    public static boolean isPalindrome(String str) {
+        if (str == null)
+            return false;
+
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i++) != str.charAt(j--)) return false;
+        }
+
+        return true;
+    }
 }
